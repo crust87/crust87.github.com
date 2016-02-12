6cf096fb1a267bad121da0f08e1f0ffe586f16bc
mabi <crust87@gmail.com>
2016-02-12 13:05:35 +0900

    add update

---
+++ data/log.js
@@ -0,0 +1,42 @@
+$logContents = [
+	{
+		type: "Branche",
+		name: "Android-VideoCropView",
+		version: "1.2.1",
+		date: "2016. 02. 11.",
+		content: "Master branche has some problem with seeking video since using MediaPlayer class. so I decide to add another VideoCropView using ExoPlayer.<br/>This version is test version of VideoCropView using ExoPlayer. It no provide all of functions in ExoPlayer but It will soon. see <a href='https://github.com/google/ExoPlayer' target='_blank'>ExoPlayer</a> if you want to know more information.",
+		link: "https://github.com/crust87/Android-VideoCropView/tree/exoplayer"
+	},
+	{
+		type: "Update",
+		name: "Android-TextureVideoView",
+		version: "1.0.1",
+		date: "2016. 02. 11.",
+		content: "setScaleType() is available change scaleType when video already playing.<br/>and update sample application.",
+		link: "https://github.com/crust87/Android-TextureVideoView"
+	},
+	{
+		type: "Create",
+		name: "Android-TextureVideoView",
+		version: "1.0.0",
+		date: "2016. 02. 10.",
+		content: "New library project TextureVideoView has been uploaded to github.<br/>This VideoView contains method for ScaleType.",
+		link: "https://github.com/crust87/Android-TextureVideoView"
+	},
+	{
+		type: "Update",
+		name: "Android-VideoTrackView",
+		version: "1.2.1",
+		date: "2016. 02. 09.",
+		content: "Fix crash on setVideo. It because call setVideo before view created.",
+		link: "https://github.com/crust87/Android-VideoTrackView"
+	},
+	{
+		type: "Update",
+		name: "Android-ImageCropper",
+		version: "1.7.1",
+		date: "2016. 02. 09.",
+		content: "add method getOriginalImage()",
+		link: "https://github.com/crust87/Android-ImageCropper"
+	}
+]
